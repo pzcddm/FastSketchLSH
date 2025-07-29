eed5d2755c568d59084e1d511e49bb308cdedfc4
HuitongJin <2019151067@email.szu.edu.cn>
2025-07-29 17:21:09 +0800

    Organize file directories

--- cpp_src/test/test_cmins_sketch.py
+++ cpp_src/test/test_cmins_sketch.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from FastSketchLSH import CMinHashSketch
 from simulation.util import estimate_jaccard, actual_jaccard
