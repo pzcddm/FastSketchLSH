88bfeab7e73c7e39ec9cda8d812dd04d239046af
pzcddm <786858269@qq.com>
2025-08-01 10:48:55 +0800

    tweak: Remove fast_sketch_numpy.py and rename rmins-sketch.py to rmins_sketch.py. Delete test_numpy.py as it is no longer needed.

--- test/test_cmins_sketch.py
+++ test/test_cmins_sketch.py
@@ -1,24 +1,26 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-from src.cmins_sketch import CMinHashSketch
+# from src.cmins_sketch import CMinHashSketch
+from src.cmins_rensa_sketch import CMinHashSketch
 from simulation.util import estimate_jaccard, actual_jaccard
 
 
 if __name__ == '__main__':
     t = 256
-    A = set(range(0, 1000))
-    B = set(range(500, 1500))
+    A = set(range(0, 100000))
+    B = set(range(50000, 150000))
 
     print(f"|A| = {len(A)}, |B| = {len(B)}")
     true_j = actual_jaccard(A, B)
     print(f"True Jaccard: {true_j:.4f}")
 
-    sketcher = CMinHashSketch()
+    sketcher_1 = CMinHashSketch()
+    sketcher_2 = CMinHashSketch()
     print("Generating sketches...")
-    S_A = sketcher.sketch(A)
-    S_B = sketcher.sketch(B)
+    S_A = sketcher_1.sketch(A)
+    S_B = sketcher_2.sketch(B)
     print("Done.")
 
     est_j = estimate_jaccard(S_A, S_B)
